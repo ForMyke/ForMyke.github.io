8ee12bf330ad125237544746b7d4e0cdc18bbb70
ForMyke <golomian16@hotmail.com>
2024-10-13 11:34:29 -0600

    form changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { threeFigureExpanded } from "./ThreeFigureExpanded.js";
+import { initializeFormBehavior } from "./scroll.js"; // Importa solo lo necesario
 
 //Renderizado usando rollup para los modulos
 document.addEventListener("DOMContentLoaded", () => {
   threeFigureExpanded(0xffffff);
+  initializeFormBehavior();
 });
---
+++ src/scroll.js
@@ -0,0 +1,56 @@
+export function initializeFormBehavior() {
+  // Evento para hacer scroll lento hacia el formulario de contacto
+  document
+    .getElementById("contact-button")
+    .addEventListener("click", function () {
+      document.getElementById("contact-form").scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+
+  // Evento del formulario para mostrar SweetAlert, validar y limpiar el formulario
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault(); // Evita el envío inmediato del formulario
+
+      // Obtener valores de los campos
+      const name = document.getElementById("name").value.trim();
+      const email = document.getElementById("email").value.trim();
+      const message = document.getElementById("message").value.trim();
+
+      // Validar que los campos no estén vacíos
+      if (!name || !email || !message) {
+        Swal.fire({
+          icon: "error",
+          title: "Algo anda mal",
+          text: "Por favor completa todos los campos!",
+        });
+        return; // Si falta algún campo, se detiene el proceso
+      }
+
+      let form = document.getElementById("contact-form"); // Referencia al formulario
+      let timerInterval;
+      Swal.fire({
+        title: "¡Mensaje enviado!",
+        html: "Redirigiendo a tu GitHub en <b></b> milisegundos.",
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading();
+          const timer = Swal.getHtmlContainer().querySelector("b");
+          timerInterval = setInterval(() => {
+            timer.textContent = Swal.getTimerLeft();
+          }, 100);
+        },
+        willClose: () => {
+          clearInterval(timerInterval);
+        },
+      }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.timer) {
+          form.reset(); // Limpiar el formulario después de enviar
+          window.location.href = "https://github.com/ForMyke"; // Redirigir a tu GitHub
+        }
+      });
+    });
+}
